ffd424645d5b698f62675d96ee7febe3c314fc07
jithendra <2200030165@kluniversity.in>
2024-12-08 05:33:05 +0530

    Update

--- pdf/src/main/java/com/example/pdf/Reposi/MarksJpaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.pdf.Reposi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.pdf.Model.Marks;
-
-public interface MarksJpaRepo extends  JpaRepository<Marks,Long>{
-    
-}
--- pdf/src/main/java/com/example/pdf/Reposi/UserJpaRepo.java
+++ pdf/src/main/java/com/example/pdf/Reposi/UserJpaRepo.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.pdf.Model.UserModel;
 public interface  UserJpaRepo extends JpaRepository<UserModel, Integer>{
     UserModel findByUserID(long userID);
+    
+
 }
